967a54aac3dd0479b8624a19feacf4d53447f612
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-06-30 18:45:57 +0300

    Intro OOP

--- lesson_1/lib/main.dart
+++ lesson_1/lib/main.dart
@@ -1,5 +1,35 @@
-void main(){
+void main() {
+  Human Ahmed = Human();
 
 
-  
+  Ahmed.numberOfArms = 2;
+  Ahmed.height = 176;
+  Ahmed.width = 40;
+  Ahmed.booldType = "A";
+
+  print(Ahmed.width);
+  print(Ahmed.booldType);
+  print(Ahmed.height);
+  print(Ahmed.numberOfArms);
+
+
+  Human Ali = Human();
+
+
+  Ali.numberOfArms = 1;
+  Ali.height = 155;
+  Ali.width = 43;
+  Ali.booldType = "B";
+
+  print(Ali.width);
+  print(Ali.booldType);
+  print(Ali.height);
+  print(Ali.numberOfArms);
+}
+
+class Human {
+  int? numberOfArms;
+  double? height;
+  double? width;
+  String? booldType;
 }
