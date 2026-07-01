2d3d64273e35e3006e5ac5961d2962db210d3c2e
Ahmed Salem <ahmedsalem2002m@gmail.com>
2026-07-01 11:33:49 +0300

    Setter

---
+++ lesson_5/lib/main.dart
@@ -0,0 +1,12 @@
+import 'human.dart';
+
+void main() {
+  Human ahmed = Human(height: 176, width: 33, booldType: "A");
+
+  ahmed.setNumbserOfArms(3);
+
+
+  ahmed.numberOfArms = 1;
+  
+  print(ahmed.getNumbserOfArms());
+}
